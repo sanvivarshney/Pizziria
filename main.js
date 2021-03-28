ecc7c6e218e4ed950b6b9cc7b637020a627b3546
sanvivarshney <76217397+sanvivarshney@users.noreply.github.com>
2021-03-28 21:43:15 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,31 @@
+menu_list_array = [
+    "Chicken Tandori Pizza",
+    "Veg Supreme Pizza",
+    "Paneer Tikka Pizaa",
+    "Deluxe Veggie Pizza",
+    "Veg Extravaganza Pizza"
+];
 
-function getParagrph1()
-{
-    var inputs=[];
-    for (var i= 1; i<6; i++)
-    {
-        inputs.push(document.getElementById("para1_input_" + i).value);
+function getmenu(){
+    var htmldata;
+    htmldata = "<ol class='menulist'>";
+    menu_list_array.sort();
+    for(var i=0; i<menu_list_array.length; i++) {
+        htmldata=htmldata+'<li>'+ menu_list_array[i] + '</li>'
     }
-    document.getElementById("showPara1").innerHTML = inputs.join(". ");
+    htmldata = htmldata + "</ol>"
+    document.getElementById("display_menu").innerHTML = htmldata;
 }
 
-
-function getParagrph2()
-{
-    var inputs=[];
-    for (var i= 1; i<6; i++)
-    {
-        inputs.push(document.getElementById("para2_input_" + i).value);
+function add_item() {
+    var htmldata;
+    var item=document.getElementById("add_item").value;
+    menu_list_array.push(item);
+    menu_list_array.sort();
+    htmldata = "<section class='cards'>"
+    for(var i=0; i<menu_list_array.length;i++) {
+        htmldata = htmldata + '<div class="card">'+'<img src="images/pizzaImg.png"/>' + menu_list_array[i] + '</div>'
     }
-    document.getElementById("showPara2").innerHTML = inputs.join(". ");
+    htmldata = htmldata + "</section>"
+    document.getElementById("discplay_addedmenu").innerHTML = htmldata;
 }
